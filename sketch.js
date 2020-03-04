c48ae4c43be10a2ecd64ec711af511a1e06a7eef
saathvik-345 <shanthimani278@gmail.com>
2020-03-04 20:38:07 +0530

    https://github.com/saathvik-354/multi-car.git

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-var ball;
+var ball,database,position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
@@ -24,6 +27,19 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x':position.x + x,
+        'y':position.y + y,
+        
+    })
+
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x ;
+    ball.y = position.y; 
+}
+function showError(){
+  console.log('error in writing in the database');
 }
